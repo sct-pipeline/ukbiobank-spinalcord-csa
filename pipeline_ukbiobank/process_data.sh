16728f72eb9ae61f99a67fb8c29884d88c76087f
sandrine bédard <sandrine.bedard@polymtl.ca>
2020-11-03 00:16:53 -0500

    Add pipeline basis, get_subject_info.py

--- pipeline_ukbiobank/process_data.sh
+++ pipeline_ukbiobank/process_data.sh
@@ -12,10 +12,11 @@
 # Authors: Sandrine Bédard, Julien Cohen-Adad
 
 #TO COMMENT when using sct_run_batch
-PATH_DATA_PROCESSED="~/data_processed"
-PATH_RESULTS="~/results"
-PATH_LOG="~/log"
-PATH_QC="~/qc"
+PATH_DATA="/mnt/c/Users/sb199/data_BIDS1" #delete wehn usinge sct_run_batch
+PATH_DATA_PROCESSED=~/ukbiobank_results
+PATH_RESULTS=/$PATH_DATA_PROCESSED/results
+PATH_LOG=/$PATH_DATA_PROCESSED/log
+PATH_QC=/$PATH_DATA_PROCESSED/qc
 
 set -x
 # Immediately exit if error
@@ -84,10 +85,10 @@ segment_if_does_not_exist(){
 # SCRIPT STARTS HERE
 # ==============================================================================
 # Display useful info for the log, such as SCT version, RAM and CPU cores available
-sct_check_dependencies -short
+#sct_check_dependencies -short Short no available for me
 
 # Go to folder where data will be copied and processed
-cd $PATH_DATA_PROCESSED
+cd ${PATH_DATA_PROCESSED}
 # Copy list of participants in processed data folder
 if [[ ! -f "participants.tsv" ]]; then
   rsync -avzh $PATH_DATA/participants.tsv .
@@ -101,5 +102,52 @@ rsync -avzh $PATH_DATA/$SUBJECT .
 # Go to anat folder where all structural data are located
 cd ${SUBJECT}/anat/
 
+# T1w
+# ------------------------------------------------------------------------------
+file_t1="${SUBJECT}_T1w"
+# Reorient to RPI and resample to 1mm iso (supposed to be the effective resolution)
+sct_image -i ${file_t1}.nii.gz -setorient RPI -o ${file_t1}_RPI.nii.gz
+sct_resample -i ${file_t1}_RPI.nii.gz -mm 1x1x1 -o ${file_t1}_RPI_r.nii.gz
+file_t1="${file_t1}_RPI_r"
 
+#ADD gradient correction here
+#file_t1="${file_t1}_gardcorr"
 
+# Segment spinal cord (only if it does not exist)
+segment_if_does_not_exist $file_t1 "t1"
+file_t1_seg=$FILESEG
+
+# Create mid-vertebral levels in the cord (only if it does not exist)
+#label_if_does_not_exist ${file_t1} ${file_t1_seg} PROBLÈME ICI
+#file_label=$FILELABEL
+# Register to PAM50 template
+#sct_register_to_template -i ${file_t1}.nii.gz -s ${file_t1_seg}.nii.gz -l ${file_label}.nii.gz -c t1 -param step=1,type=seg,algo=centermassrot:step=2,type=seg,algo=syn,slicewise=1,smooth=0,iter=5:step=3,type=im,algo=syn,slicewise=1,smooth=0,iter=3 -qc ${PATH_QC} -qc-subject ${SUBJECT}
+# Rename warping fields for clarity
+#mv warp_template2anat.nii.gz warp_template2T1w.nii.gz
+#mv warp_anat2template.nii.gz warp_T1w2template.nii.gz
+#Warp template without the white matter atlas (we don't need it at this point)
+#sct_warp_template -d ${file_t1}.nii.gz -w warp_template2T1w.nii.gz -a 0 -ofolder label_T1w
+#Generate QC report to assess vertebral labeling
+#sct_qc -i ${file_t1}.nii.gz -s label_T1w/template/PAM50_levels.nii.gz -p sct_label_vertebrae -qc ${PATH_QC} -qc-subject ${SUBJECT}
+# Flatten scan along R-L direction (to make nice figures)
+#sct_flatten_sagittal -i ${file_t1}.nii.gz -s ${file_t1_seg}.nii.gz
+# Compute average cord CSA between C2 and C3
+#sct_process_segmentation -i ${file_t1_seg}.nii.gz -vert 2:3 -vertfile label_T1w/template/PAM50_levels.nii.gz -o ${PATH_RESULTS}/csa-SC_T1w.csv -append 1
+
+# T2
+# ------------------------------------------------------------------------------
+file_t2="${SUBJECT}_T2w"
+# Reorient to RPI and resample to 0.8mm iso (supposed to be the effective resolution)
+sct_image -i ${file_t2}.nii.gz -setorient RPI -o ${file_t2}_RPI.nii.gz
+sct_resample -i ${file_t2}_RPI.nii.gz -mm 0.8x0.8x0.8 -o ${file_t2}_RPI_r.nii.gz
+file_t2="${file_t2}_RPI_r"
+# Segment spinal cord (only if it does not exist)
+segment_if_does_not_exist $file_t2 "t2"
+file_t2_seg=$FILESEG
+# Flatten scan along R-L direction (to make nice figures)
+sct_flatten_sagittal -i ${file_t2}.nii.gz -s ${file_t2_seg}.nii.gz
+
+# Bring vertebral level into T2 space PLus tard puisque impossible pour T1
+#sct_register_multimodal -i label_T1w/template/PAM50_levels.nii.gz -d ${file_t2_seg}.nii.gz -o PAM50_levels2${file_t2}.nii.gz -identity 1 -x nn
+# Compute average cord CSA between C2 and C3
+#sct_process_segmentation -i ${file_t2_seg}.nii.gz -vert 2:3 -vertfile PAM50_levels2${file_t2}.nii.gz -o ${PATH_RESULTS}/csa-SC_T2w.csv -append 1
