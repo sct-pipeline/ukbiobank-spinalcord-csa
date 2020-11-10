74c27b4a1a0194170242f73525a722b0a21d1f19
sandrine bédard <sandrine.bedard@polymtl.ca>
2020-11-09 20:02:14 -0500

    completed writing and testing of script locally

--- pipeline_ukbiobank/cli/select_subjects.py
+++ pipeline_ukbiobank/cli/select_subjects.py
@@ -10,41 +10,43 @@ import pipeline_ukbiobank
 import csv
 import yaml
 import pandas as pd
-import numpy as np
 
 def get_parser():
     parser = argparse.ArgumentParser(
-        description="Genrates a selected subjects list (.yml) for the parameters who are available",
+        description="Genrates a selected subjects list (.yml). A subject is selected if it has data for all the chosen parameters",
         prog=os.path.basename(__file__).strip('.py')
         )
     parser.add_argument('-subjects_datafile', required=True, type=str,
-                        help="Name of the .csv file containing all the subject info.")
-
+                        help="Name of the .csv file containing all the subjects feilds data.")
+    parser.add_argument('-parameters', required=True, type=str,
+                        help="Name of the .txt file containing all the selelected parameters' feild id.")                    
     return parser
 
+def load_participant_data_file(filename):
+    """
+    Load the participaant datafile and build pandas DF of participants
+    :return:data: pandas dataframe
+    """
+    data = pd.read_csv(filename)
+    return data
 
 def main ():
     parser = get_parser()
     args = parser.parse_args()
-    #TODO read from the export_feilds_GBM3100
-    sexe_id = "31"
-    birth_month_id = "52"
-    birth_year_id = "34"
-    height_id = "12144-2.0"
-    weight_id = "21002-2.0"
-    icv_id = "25010-2.0"
-    #Load subject data file
-    dict_results = []
-    with open(args.subjects_datafile, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-                dict_results.append(row)
-    #Create a structure
-    df = pd.DataFrame.from_dict(dict_results, orient='index')
-    print(df[0:10])
-#1. create a yml list with all subjects
-#2. Goes through the data file and returns a list of eid that have a data not available
-#3. Removes the subjects eid from the list at 2
+    #Load the .txt file containing the feild ID of each chosen parameter
+    with open(args.parameters, 'r') as f:
+        parameters = f.read().splitlines()
+    #Load the .csv file containing all the data of the subjects
+    df = load_participant_data_file(args.subjects_datafile)
+    #Select the data for the chosen parameters
+    df_short = df[parameters]
+    #Removes a subject if it has an empty feild among the parameters
+    df_updated = df_short.dropna(0,how = 'any').reset_index(drop=True)
+    #Generates a list of the remaining subjects' eid 
+    select_subjects = df_updated[parameters[0]].to_list()
+    #Writes a .yml file with the selected subjects
+    with open('selected_subjects.yml', 'w') as f:
+        yaml.dump(select_subjects, f)
 
 if __name__ == '__main__':
     main()
