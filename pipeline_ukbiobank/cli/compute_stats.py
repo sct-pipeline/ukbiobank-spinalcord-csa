7cec3fe0d8e2094dcbca00cb25dc159d75d638d5
sandrine bédard <sandrine.bedard@polymtl.ca>
2020-11-21 19:42:59 -0500

    stepwise models summary

--- pipeline_ukbiobank/cli/compute_stats.py
+++ pipeline_ukbiobank/cli/compute_stats.py
@@ -12,25 +12,28 @@ import pandas as pd
 import numpy as np
 import logging
 import sys
+import yaml
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
+
 from sklearn.feature_selection import RFECV
 from sklearn.svm import SVR
 #import seaborn as sns
 
-import pipeline_ukbiobank.cli.select_subjects as ss
+#import pipeline_ukbiobank.cli.select_subjects as ss
 from sklearn.linear_model import LinearRegression
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
 
 FNAME_LOG = 'log_stats.txt'
+
 # Initialize logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)  # default: logging.DEBUG, logging.INFO
 hdlr = logging.StreamHandler(sys.stdout)
 logging.root.addHandler(hdlr)
 
-PREDICTORS = ['Sex', 'Height', 'Weight', 'Intracranial volume', 'Age']
+PREDICTORS = ['Sex', 'Height', 'Weight', 'Intracranial volume', 'Age'] # Add units of each
 
 def get_parser():
     parser = argparse.ArgumentParser(
@@ -38,17 +41,25 @@ def get_parser():
         prog=os.path.basename(__file__).strip('.py')
         )
     parser.add_argument('-path-results',
-                        required=False,
+                        required=True,
                         metavar='<dir_path>',
-                        help="Folder that includes the output csv file from get_subkects_info")
-    parser.add_argument('-exclude',
+                        help="Folder that includes the output csv file from get_subjects_info")
+    parser.add_argument('-dataFile',
                         required=False,
-                        help=".yml list of subjects to exclude from statistical analysis.") #add format of the list
+                        default='data_ukbiobank.csv',
+                        metavar='<file>',
+                        help="Filename of the data, output file of uk_get_subject_info, default: data_ukbiobank.csv  ")
+    parser.add_argument('-exclude',
+                        metavar='<file>',
+                        required=False,
+                        help=".yml list of subjects to exclude from statistical analysis."
+                        ) #add format of the list
 
     return parser
-FILENAME = 'data_ukbiobank.csv'
 
-#0. remove subjects
+FILENAME = 'data_ukbiobank_test.csv'
+
+#0. remove subjects --> OK
 #1. Caractérisation des données
     #Nombre de sujet --> OK
     #Moyenne --> OK
@@ -58,20 +69,23 @@ FILENAME = 'data_ukbiobank.csv'
     #Int confiance (après je crois) --> OK
     # % H vs F --> OK
     # Plage de données pour taille, volume, poids, age --> OK
-    #OUTPUT tableau + CSV (AJOUTER)
+    #OUTPUT tableau + CSV (AJOUTER) ajouter unités
 #2. Coff corrélation --> OK
     #OUTPUT tableau + CSV 
 #3 Multivariate regression (stepwise)
-    #output coeff.txt  
+    #normalise data before ???
+    #full and stepwise model --> ok, ajouter AIC
+    #output coeff.txt   --> ok for stepwise
 #4 Test tuckey diff T1w et T2w
 #5 Pertinence modèle
-    #collinéarité (scatterplot avec age et height)
-    #Résidus
-    #R^2
-    #Analyse variance
-    #Intervalle de prédiction
-#6 Logfile
-# add exclude parameter
+    # collinéarité (scatterplot avec age et height)
+    # Résidus
+    # R^2 --> ok, faire un texte
+    # Analyse variance
+    # Intervalle de prédiction
+#6 Logfile --> OK, mais en ajouter partout
+# TODO: gérer l'écriture des fichiers
+
 def compute_statistics(df):
     """
     Compute statistics such as mean, std, COV, etc. per contrast type
@@ -116,7 +130,13 @@ def compute_predictors_statistic(df):
     stats['Sex'] = {}
     stats['Sex']['%_M'] = 100*(np.count_nonzero(df['Sex']) / len(df['Sex']))
     stats['Sex']['%_F'] = 100 - stats['Sex']['%_M']
-    
+    # Writes 
+    output_text_stats(stats)
+    return stats
+def output_text_stats(stats):
+    """
+    Embed statistical results into sentences so they can easily be copy/pasted into a manuscript.
+    """
     logger.info('Sex statistic:')
     logger.info('{} % of male  and {} % of female.'.format(stats['Sex']['%_M'],
                                                                              stats['Sex']['%_F']))
@@ -140,9 +160,7 @@ def compute_predictors_statistic(df):
                                                                             stats['Intracranial volume']['max'],
                                                                             stats['Intracranial volume']['med'],
                                                                             stats['Intracranial volume']['mean']))
-    return stats
-
-def config_table(corr_table, filename):
+def config_table(corr_table, filename): # add units to table
     plt.figure(linewidth=2,
            tight_layout={'pad':1},
            figsize=(15,4)
@@ -173,26 +191,28 @@ def config_table(corr_table, filename):
             edgecolor=fig.get_edgecolor(),
             facecolor=fig.get_facecolor(),
             )
+    logger.info('Created: ' + filename)
 
 
 def get_correlation_table(df) :
     corr_table = df.corr()
     return corr_table
 
-def compute_regression(df):
-    #model = ols("T1w_CSA ~ Sex + Age + Weight + Height ", data=df)
-    #results = model.fit()
-    #print(results.params)
-    x = df.drop(columns = ['T1w_CSA', 'T2w_CSA'])
-    #x = sm.add_constant(x)
-    y_T1w = df['T1w_CSA']
-    #model = sm.OLS(y_T1w, x)
-    #results = model.fit()
+def get_predictors_rank(x,y):
+
     estimator = SVR(kernel="linear")
     selector = RFECV(estimator, step=1, cv=5)
     selector = selector.fit(x, y_T1w)
-    print(selector.ranking_)
-    #print(results.summary())
+    return selector.ranking_
+
+def generate_linear_model(x, y, selected_features):
+    x = x[selected_features]
+    x = sm.add_constant(x)
+
+    model = sm.OLS(y, x)
+    results = model.fit()
+    return results
+
 #def get_scatterplot(x):
     #sns.pairplot(x)
 def compute_stepwise(X,y, threshold_in, threshold_out): # TODO: add AIC cretaria
@@ -200,13 +220,13 @@ def compute_stepwise(X,y, threshold_in, threshold_out): # TODO: add AIC cretaria
     Performs backword and forward feature selection based on p-values 
     
     Args:
-        X: panda.DataFrame with the candidate predictors
-        y: panda.DataFrame with the candidate predictors with target
+        X (panda.DataFrame): Candidate predictors
+        y (panda.DataFrame): Candidate predictors with target
         threshold_in: include a feature if its p-value < threshold_in
         threshold_out: exclude a feature if its p-value > threshold_out
 
     Retruns:
-        selected_features: list of selected features
+        included: list of selected features
     
     """
     included = []
@@ -226,7 +246,7 @@ def compute_stepwise(X,y, threshold_in, threshold_out): # TODO: add AIC cretaria
             best_feature = excluded[new_pval.argmin()] # problème d'ordre ici --> OK!!
             included.append(best_feature)
             changed=True
-            print('Add  {:30} with p-value {:.6}'.format(best_feature, best_pval))
+            logger.info('Add  {:30} with p-value {:.6}'.format(best_feature, best_pval))
 
         # backward step
         model = sm.OLS(y, sm.add_constant(pd.DataFrame(X[included]))).fit()
@@ -239,45 +259,133 @@ def compute_stepwise(X,y, threshold_in, threshold_out): # TODO: add AIC cretaria
             #print('worst', worst_feature)
             included.remove(worst_feature)
             if verbose:
-                print('Drop {:30} with p-value {:.6}'.format(worst_feature, worst_pval))
+                logger.info('Drop {:30} with p-value {:.6}'.format(worst_feature, worst_pval))
         if not changed:
             break
     return included
 
+def compute_regression_CSA(x,y, p_in, p_out, contrast):
+    
+    logger.info("Stepwise linear regression {}:".format(contrast))
+    selected_features = compute_stepwise(x, y, p_in, p_out)
+    
+    # Generates model for T1w et T2w with p-value stepwise linear regression
+    model = generate_linear_model(x,y, selected_features)
+    
+    # TODO : faire un fonction qui formate tout et écrit tous les fichier
+    with open('summary_stepwise'+ contrast + '.txt', 'w') as fh: # Modifier le lieu, faire un dossier
+        fh.write(model.summary(title = 'Stepwise linear regression of ' + contrast).as_text())
+    (model.params).to_csv('coeff_stepwise'+ contrast + '.csv', header = None)
+
+
+    #Generates liner regression with all predictors
+    model_full = generate_linear_model(x,y, PREDICTORS)
+
+    with open('summary_full_'+ contrast + '.txt', 'w') as fh: # Modifier le lieu, faire un dossier
+        fh.write(model_full.summary(title = 'Full linear regression of ' + contrast).as_text())
+    (model.params).to_csv('coeff_stepwise'+ contrast + '.csv', header = None)
+    #Compares full and reduced models
+    compared_models = compare_models(model, model_full, 'Stepwise_model', 'Full model')
+
+
+def compare_models(model_1, model_2,model_1_name, model_2_name  ):
+    columns = ['Model', 'R^2', 'R^2_adj', 'F_p-value','F_value', 'AIC', 'df_res'] # pas sur si nécessaire
+    table = pd.DataFrame(columns= columns)
+    table['Model'] = [model_1_name, model_2_name]
+    table['R^2'] = [model_1.rsquared, model_2.rsquared]
+    table['R^2_adj'] = [model_1.rsquared_adj, model_2.rsquared_adj]
+    table['F_p-value'] = [model_1.f_pvalue, model_2.f_pvalue]
+    table['F_value'] = [model_1.fvalue, model_2.fvalue]
+    table['AIC'] = [model_1.aic, model_2.aic]
+    table['df_res'] = [model_1.df_resid, model_2.df_resid]
+    
+    return table
+
+def remove_subjects(df, dict_exclude_subj):
+    """
+    Removes subjects from exclude list if given and all subjects that are missing any parameter.
+    Writes in log list of removed subjects.
+    Args:
+        df (panda.DataFrame): Dataframe with all subjects.
+    Returns
+        df_updated (panda.DataFrame): Dataframe with subjects removed.
+    """
+    subjects_removed = df.loc[pd.isnull(df).any(1), :].index.values
+    df = df.drop(index = dict_exclude_subj)
+    df_updated = df.dropna(0,how = 'any').reset_index(drop=True)
+    subjects_removed = np.append(subjects_removed,dict_exclude_subj)
+    logger.info("Subjects removed: {}".format(subjects_removed))
+    return df_updated
+
 def main():
     parser = get_parser()
     args = parser.parse_args()
-    df = (ss.load_participant_data_file(args.path_results +'/' + FILENAME)).set_index('Subject')
 
+    # Creates a panda dataFrame from data file .csv 
+    df = (pd.read_csv(args.path_results +'/' + args.dataFile)).set_index('Subject')
+    
+    #create dict with subjects to exclude if input yml config file is passed
+    if args.exclude is not None:
+        #check if input yml file exists
+        if os.path.isfile(args.exclude):
+            fname_yml = args.exclude
+        else:
+            sys.exit("ERROR: Input yml file {} does not exist or path is wrong.".format(args.exclude))
+        with open(fname_yml, 'r') as stream:
+            try:
+                dict_exclude_subj = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                logger.error(exc)
+    else:
+        # initialize empty dict if no config yml file is passed
+        dict_exclude_subj = dict()
+
+    # Dump log file there
+    if os.path.exists(FNAME_LOG):
+        os.remove(FNAME_LOG)
+    fh = logging.FileHandler(os.path.join(os.path.abspath(os.curdir), FNAME_LOG))
+    logging.root.addHandler(fh)
+#_______________________________________________________________________________________________________ 
+#0. Removes all subjects that are missing a parameter or CSA value and subjects from exclude list.
+    df = remove_subjects(df, dict_exclude_subj) 
+
+# Initialize path of results figues
+    path_figures = args.path_results + '/figures'
+    if not os.path.isdir(path_figures):
+        os.mkdir(args.path_results + '/figures')
+# _____________________________________________________________________________________________________
 #1. Compute stats
+
     #1.1 Compute stats of T1w CSA and T2w CSA
     stats_csa = compute_statistics(df)
     stats_csa_df = pd.DataFrame.from_dict(stats_csa)
-    config_table(stats_csa_df,  args.path_results + '/stats_CSA.png' )
+    # Format and save stats of csa as a table
+    config_table(stats_csa_df, path_figures + '/stats_CSA.png' ) # add units to table
+
     #add save as csv file
+
     #1.2. Compute stats of the predictors
     stats_predictors = compute_predictors_statistic(df)
+    stats_predictors_df = pd.DataFrame.from_dict(stats_predictors)
+
+    # Format and save stats of csa as a table
+    config_table(stats_predictors_df, path_figures + '/stats_param.png' )
     #add save as csv file and .png figure
+#_______________________________________________________________________________________________________ 
 #2. Correlation matrix
     corr_table = get_correlation_table(df)
+    logger.info("Correlation matrix: {}".format(corr_table))
     # Saves an .png of the correlation matrix in the results folder
-    path_figures = args.path_results + '/figures'
-
-    if not os.path.isdir(path_figures):
-        os.mkdir(args.path_results + '/figures')
     config_table(corr_table, path_figures+ '/corr_table.png')
-#3 Stepwise linear regression
-    compute_regression(df)
+#________________________________________________________________________________________________________    
+#3 Stepwise linear regression | Faire un fonction avec tout ça
     x = df.drop(columns = ['T1w_CSA', 'T2w_CSA'])
     y_T1w = df['T1w_CSA']
     y_T2w = df['T2w_CSA']
-    p_in = 0.15
-    p_out = 0.15
-   
-   # print(x.shape, y_T1w.shape)
-    included= compute_stepwise(x, y_T1w, p_in, p_out)
-    print('Included',included)
-
+    p_in = 0.25 # to check
+    p_out = 0.25 # To check
+    compute_regression_CSA(x, y_T1w, p_in, p_out, "T1w_CSA")
+    compute_regression_CSA(x, y_T2w, p_in, p_out, 'T2w_CSA')
 
 if __name__ == '__main__':
     main()
