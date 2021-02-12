7045962d4afdb08ae90aca1794567ebbdb102237
Sandrine <sandrine.bedard@polymtl.ca>
2021-02-11 21:25:11 -0500

    adjust mem

--- job_template.sh
+++ job_template.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=0-02:00        # time (DD-HH:MM)
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=32    # number of OpenMP processes
-#SBATCH --mem=30G
+#SBATCH --mem=128G
 #SBATCH --mail-user=sandrine.bedard@polymtl.ca
 #SBATCH --mail-type=ALL
 export OMP_NUM_THREADS=1
