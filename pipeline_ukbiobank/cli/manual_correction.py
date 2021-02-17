24756da689aa965b62881638c5d7c5f2240c963f
sandrinebedard <71230552+sandrinebedard@users.noreply.github.com>
2021-02-17 16:34:29 -0500

    Update pipeline_ukbiobank/cli/manual_correction.py
    Co-authored-by: Julien Cohen-Adad <jcohen@polymtl.ca>

--- pipeline_ukbiobank/cli/manual_correction.py
+++ pipeline_ukbiobank/cli/manual_correction.py
@@ -43,7 +43,7 @@ def get_parser():
         "R|Config yaml file listing images that require manual corrections for segmentation and vertebral "
         "labeling. 'FILES_SEG' lists images associated with spinal cord segmentation "
         "and 'FILES_LABEL' lists images associated with vertebral labeling. "
-        "You can validate your yaml file at this website: http://www.yamllint.com/."
+        "You can validate your .yml file at this website: http://www.yamllint.com/."
         " If you want to correct segmentation only, ommit 'FILES_LABEL' in the list. Below is an example yaml file:\n"
         + dedent(
             """
