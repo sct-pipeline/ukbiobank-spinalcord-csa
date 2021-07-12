50affff21e8d70c2125dc1fb2e4785a927c51c83
sandrinebedard <71230552+sandrinebedard@users.noreply.github.com>
2021-07-12 12:07:19 -0400

    Add manual labeling of PMJ in `manual_correction.py` (#82)
    * add PMJ detection and csa measure from PMJ
    
    * change end of lines
    
    * fix typo
    
    * add check for manual pmj label
    
    * add task FILES_PMJ
    
    * add function to create viewer to label PMJ
    
    * fix syntax
    
    * add QC for PMJ
    
    * Add manual labeling of PMJ instructions

--- pipeline_ukbiobank/cli/manual_correction.py
+++ pipeline_ukbiobank/cli/manual_correction.py
@@ -14,7 +14,6 @@ import json
 import os
 import sys
 import shutil
-import subprocess
 from textwrap import dedent
 import time
 import yaml
@@ -30,7 +29,7 @@ def get_parser():
     parser function
     """
     parser = argparse.ArgumentParser(
-        description='Manual correction of spinal cord segmentation and vertebral labeling. '
+        description='Manual correction of spinal cord segmentation, vertebral and pontomedullary junction labeling. '
                     'Manually corrected files are saved under derivatives/ folder (BIDS standard).',
         formatter_class=utils.SmartFormatter,
         prog=os.path.basename(__file__).strip('.py')
@@ -42,7 +41,8 @@ def get_parser():
         help=
         "R|Config yaml file listing images that require manual corrections for segmentation and vertebral "
         "labeling. 'FILES_SEG' lists images associated with spinal cord segmentation "
-        "and 'FILES_LABEL' lists images associated with vertebral labeling. "
+        ",'FILES_LABEL' lists images associated with vertebral labeling "
+        "and 'FILES_PMJ' lists images associated with pontomedullary junction labeling"
         "You can validate your .yml file at this website: http://www.yamllint.com/."
         " If you want to correct segmentation only, ommit 'FILES_LABEL' in the list. Below is an example .yml file:\n"
         + dedent(
@@ -51,6 +51,9 @@ def get_parser():
             - sub-1000032_T1w.nii.gz
             - sub-1000083_T2w.nii.gz
             FILES_LABEL:
+            - sub-1000032_T1w.nii.gz
+            - sub-1000710_T1w.nii.gz
+            FILES_PMJ:
             - sub-1000032_T1w.nii.gz
             - sub-1000710_T1w.nii.gz\n
             """)
@@ -95,6 +98,8 @@ def get_function(task):
         return 'sct_deepseg_sc'
     elif task == 'FILES_LABEL':
         return 'sct_label_utils'
+    elif task == 'FILES_PMJ':
+        return 'sct_detect_pmj'
     else:
         raise ValueError("This task is not recognized: {}".format(task))
 
@@ -104,6 +109,9 @@ def get_suffix(task, suffix=''):
         return '_seg'+suffix
     elif task == 'FILES_LABEL':
         return '_labels'+suffix
+    elif task == 'FILES_PMJ':
+        return '_pmj'+suffix
+
     else:
         raise ValueError("This task is not recognized: {}".format(task))
 
@@ -120,10 +128,10 @@ def correct_segmentation(fname, fname_seg_out):
     # launch ITK-SNAP
     # Note: command line differs for macOs/Linux and Windows
     print("In ITK-SNAP, correct the segmentation, then save it with the same name (overwrite).")
-    if shutil.which('itksnap') != None: # Check if command 'itksnap' exists
-        os.system('itksnap -g ' + fname + ' -s ' + fname_seg_out) # for macOS and Linux
-    elif shutil.which('ITK-SNAP') != None: # Check if command 'ITK-SNAP' exists
-        os.system('ITK-SNAP -g ' + fname + ' -s ' + fname_seg_out) # For windows
+    if shutil.which('itksnap') is not None:  # Check if command 'itksnap' exists
+        os.system('itksnap -g ' + fname + ' -s ' + fname_seg_out)  # for macOS and Linux
+    elif shutil.which('ITK-SNAP') is not None:  # Check if command 'ITK-SNAP' exists
+        os.system('ITK-SNAP -g ' + fname + ' -s ' + fname_seg_out)  # For windows
     else:
         sys.exit("ITK-SNAP not found. Please install it before using this program or check if it was added to PATH variable. Exit program.")
 
@@ -138,6 +146,18 @@ def correct_vertebral_labeling(fname, fname_label):
     """
     message = "Click at the posterior tip of the disc between C1-C2, C2-C3 and C3-C4 vertebral levels, then click 'Save and Quit'."
     os.system('sct_label_utils -i {} -create-viewer 2,3,4 -o {} -msg "{}"'.format(fname, fname_label, message))
+
+
+def correct_pmj_label(fname, fname_label):
+    """
+    Open sct_label_utils to manually label PMJ.
+    :param fname:
+    :param fname_label:
+    :param name_rater:
+    :return:
+    """
+    message = "Click at the posterior tip of the pontomedullary junction (PMJ) then click 'Save and Quit'."
+    os.system('sct_label_utils -i {} -create-viewer 50 -o {} -msg "{}"'.format(fname, fname_label, message))
 
 
 def create_json(fname_nifti, name_rater):
@@ -177,7 +197,7 @@ def main():
             dict_yml = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
-    
+
     # Curate dict_yml to only have filenames instead of absolute path
     dict_yml = utils.curate_dict_yml(dict_yml)
 
@@ -197,7 +217,7 @@ def main():
 
     # Get list of segmentations files for all subjects in -path-in (if -add-seg-only)
     if args.add_seg_only:
-        path_list = glob.glob(args.path_in + "/**/*_seg.nii.gz", recursive=True) # TODO: add other extension
+        path_list = glob.glob(args.path_in + "/**/*_seg.nii.gz", recursive=True)  # TODO: add other extension
         # Get only filenames without suffix _seg  to match files in -config .yml list
         file_list = [utils.remove_suffix(os.path.split(path)[-1], '_seg') for path in path_list]
 
@@ -205,13 +225,13 @@ def main():
     # Perform manual corrections
     for task, files in dict_yml.items():
         # Get the list of segmentation files to add to derivatives, excluding the manually corrrected files in -config.
-        if args.add_seg_only and task=='FILES_SEG':
+        if args.add_seg_only and task == 'FILES_SEG':
             # Remove the files in the -config list
             for file in files:
                 if file in file_list:
                     file_list.remove(file)
-            files = file_list # Rename to use those files instead of the ones to exclude
-        if files is not None: 
+            files = file_list  # Rename to use those files instead of the ones to exclude
+        if files is not None:
             for file in files:
                 # build file names
                 subject = file.split('_')[0]
@@ -249,19 +269,24 @@ def main():
                             if not utils.check_software_installed():
                                 sys.exit("Some required software are not installed. Exit program.")
                             correct_vertebral_labeling(fname, fname_label)
+                        elif task == 'FILES_PMJ':
+                            if not utils.check_software_installed():
+                                sys.exit("Some required software are not installed. Exit program.")
+                            correct_pmj_label(fname, fname_label)
                         else:
                             sys.exit('Task not recognized from yml file: {}'.format(task))
                         # create json sidecar with the name of the expert rater
                         create_json(fname_label, name_rater)
 
                 # generate QC report (only for vertebral labeling or for qc only)
-                if args.qc_only or task == 'FILES_LABEL':
+                if args.qc_only or task != 'FILES_SEG':
                     os.system('sct_qc -i {} -s {} -p {} -qc {} -qc-subject {}'.format(
-                        fname, fname_label, get_function(task) , fname_qc, subject))
+                        fname, fname_label, get_function(task), fname_qc, subject))
                     # Archive QC folder
                     shutil.copy(fname_yml, fname_qc)
                     shutil.make_archive(fname_qc, 'zip', fname_qc)
                     print("Archive created:\n--> {}".format(fname_qc+'.zip'))
+
 
 if __name__ == '__main__':
     main()
